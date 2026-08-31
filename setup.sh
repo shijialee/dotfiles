2aecfbb54c2f23c3b51b32958581c5a5557141be
qiang <shijialee@gmail.com>
2026-09-01 00:03:29 +1000

    ignore error for UseKeychain as it is for mac only

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ ln -fsvn ~/.dotfiles/link/.vimrc               ~/.vimrc
 ln -fsvn ~/.dotfiles/link/.vim                 ~/.vim
 ln -fsvn ~/.dotfiles/link/.gitconfig           ~/.gitconfig
 ln -fsvn ~/.dotfiles/link/.gitignore_global    ~/.gitignore_global
-ln -fsvn ~/.dotfiles/link/.ssh/config          ~/.ssh/config
+# only link for local env
+#ln -fsvn ~/.dotfiles/link/.ssh/config          ~/.ssh/config
 ln -fsvn ~/.dotfiles/link/.screenrc            ~/.screenrc
 ln -fsvn ~/.dotfiles/link/kitty                ~/.config/kitty
 ln -fsvn ~/.dotfiles/link/AutoRaise            ~/.config/AutoRaise
